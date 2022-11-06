6f71a2f4fde50f0617696645dae3ac16690ab872
Oleh Titov <oleg.titov81@gmail.com>
2022-11-06 10:32:15 +0100

    Initial Commit

---
+++ Savings Jar/Savings_JarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Savings_JarApp.swift
+//  Savings Jar
+//
+//  Created by Oleh Titov on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Savings_JarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
